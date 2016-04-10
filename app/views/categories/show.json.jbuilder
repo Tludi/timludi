b3857e2f29cb3709ea00941692ac390013017cf2
Tludi <tludi00@gmail.com>
2016-04-10 13:43:38 -0700

    added category scaffold

---
+++ app/views/categories/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @category, :id, :name, :fonticon, :created_at, :updated_at
---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :category do
+    name "MyString"
+    fonticon "MyString"
+  end
+end
